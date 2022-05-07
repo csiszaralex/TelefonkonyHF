bef064ec8d6a9058513b5b440ba3d36d33ff59ce
csiszaralex <csiszaralex@gmail.com>
2022-05-07 10:48:43 +0200

    :art: Adds another exception

--- exceptions.h
+++ exceptions.h
@@ -10,23 +10,21 @@ protected:
     int err_offset;
     std::string err_msg;
 public:
-    Exception(const std::string msg, int num, int offset): err_msg(msg), err_num(num), err_offset(offset) {}
+    Exception(const std::string msg, int num, int offset): err_num(num), err_offset(offset), err_msg(msg) {}
     virtual ~Exception() throw() {}
 
-    virtual const char* what() const throw() {
-        return err_msg.c_str();
-    }
-    virtual int getErrorNumber() const throw() {
-        return err_num;
-    }
-    virtual int getErrorOffset() const throw() {
-        return err_offset;
-    }
+    virtual const char* what() const throw() { return err_msg.c_str(); }
+    virtual int getErrorNumber() const throw() { return err_num; }
+    virtual int getErrorOffset() const throw() { return err_offset; }
 };
 
 class OutOfRangeException: public Exception {
 public:
-    OutOfRangeException(): Exception("Tulindexeles", 1, 5) {}
+    OutOfRangeException(int elerniKivantIndex, int maxIndex): Exception("Tulindexeles", 1, 5) {}
+};
+class IsEmptyException: public Exception {
+public:
+    IsEmptyException(): Exception("Ez már üres", 2, 1) {}
 };
 
 #endif //TELEFON_EXCEPTION_H
