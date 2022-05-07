137c2eef9f81aa0e30663240392d849960d690ae
csiszaralex <csiszaralex@gmail.com>
2022-05-08 00:11:18 +0200

    :lipstick: Adds exceptions for file handling

--- exceptions.h
+++ exceptions.h
@@ -7,24 +7,26 @@
 class Exception: public std::exception {
 protected:
     int err_num;
-    int err_offset;
     std::string err_msg;
 public:
-    Exception(const std::string msg, int num, int offset): err_num(num), err_offset(offset), err_msg(msg) {}
+    Exception(const std::string msg, int num): err_num(num),  err_msg(msg) {}
     virtual ~Exception() throw() {}
 
     virtual const char* what() const throw() { return err_msg.c_str(); }
     virtual int getErrorNumber() const throw() { return err_num; }
-    virtual int getErrorOffset() const throw() { return err_offset; }
 };
 
 class OutOfRangeException: public Exception {
 public:
-    OutOfRangeException(int elerniKivantIndex, int maxIndex): Exception("Tulindexeles", 1, 5) {}
+    OutOfRangeException(int elerniKivantIndex, int maxIndex): Exception("Tulindexeles", 1) {}
 };
-class IsEmptyException: public Exception {
+class FileError: public Exception {
 public:
-    IsEmptyException(): Exception("Ez már üres", 2, 1) {}
+    FileError(const char* file): Exception("File hiba", -6) {}
+};
+class ArgumentCountError: public Exception {
+public:
+    ArgumentCountError(int db): Exception("Nem megfelelő argumentum szam", -3) {}
 };
 
 #endif //TELEFON_EXCEPTION_H
