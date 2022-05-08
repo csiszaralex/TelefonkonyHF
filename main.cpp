2d2e84887ae4442e517f9e8a12824c6b1f13ef31
csiszaralex <csiszaralex@gmail.com>
2022-05-08 15:27:59 +0200

    :boom: Make main.cpp compatible with reformated classes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 
 #include "list.h"
+#include "string.h"
 #include "ember.h"
 
 int main(int argc, char *argv[]) {
@@ -8,7 +9,7 @@ int main(int argc, char *argv[]) {
     if(strcmp(argv[1], "show") != 0 && strcmp(argv[1], "add") != 0 && strcmp(argv[1], "remove") != 0) throw CommandError();
 
     List<Ember> emberek;
-    //region File
+    //region File read
     std::ifstream file;
     file.open("../adatok.dat");
     if(file.is_open()) {
@@ -16,9 +17,9 @@ int main(int argc, char *argv[]) {
         while(file >> db) {
             Ember* tmp = new Ember();
             if(db < 8 || db > 14 || (db > 9 && db < 14)) throw FileError("Hibas db szam");
-            if(db >= 8)file >> tmp->nev.vezetekNev >> tmp->nev.keresztNev >> tmp->cim.postal >> tmp->cim.city >> tmp->cim.address >> tmp->cim.type >> tmp->cim.number >> tmp->telefon;
-            if(db >= 9) file >> tmp->nev.beceNev;
-            if(db >= 14) file >> tmp->privCim.postal >> tmp->privCim.city >> tmp->privCim.address >> tmp->privCim.type >> tmp->privCim.number;
+            if(db >= 8)file >> tmp->nev >> tmp->cim >> tmp->telefon;
+            if(db >= 9) file >> tmp->nev.get_bece();
+            if(db >= 14) file >> tmp->privCim;
 
             emberek.push_back(tmp);
         }
@@ -38,7 +39,7 @@ int main(int argc, char *argv[]) {
     if(strcmp(argv[1], "remove") == 0) {
         if(argc < 3) throw ArgumentCountError(argc);
         int id = -1;
-        for (size_t i = 0; i < emberek.get_size(); ++i) {
+        for (int i = 0; i < emberek.get_size(); ++i) {
             if(emberek[i].telefon == argv[2]) {id = i;break;}
         }
         if(id == -1) throw NoPhoneNumber(argv[2]);
@@ -49,43 +50,35 @@ int main(int argc, char *argv[]) {
 
         Ember* tmp = new Ember();
         if(argc >= 10) {
-            tmp->nev.vezetekNev = argv[2];
-            tmp->nev.keresztNev = argv[3];
-            tmp->cim.postal = std::stoi(argv[4]);
-            tmp->cim.city  = argv[5];
-            tmp->cim.address = argv[6];
-            tmp->cim.type = argv[7];
-            tmp->cim.number = argv[8];
+            tmp->nev.set_nev(argv[2], argv[3]);
+            tmp->cim.set_cim(std::stoi(argv[4]), argv[5], argv[6], argv[7], argv[8]);
             tmp->telefon = argv[9];
         }
         if(argc >= 11)
-            tmp->nev.beceNev = argv[10];
+            tmp->nev.set_bece(argv[10]);
         if(argc >= 16)
         {
-            tmp->privCim.postal = std::stoi(argv[11]);
-            tmp->privCim.city  = argv[12];
-            tmp->privCim.address = argv[13];
-            tmp->privCim.type = argv[14];
-            tmp->privCim.number = argv[15];
+            tmp->privCim.set_cim(std::stoi(argv[11]), argv[12], argv[13], argv[14], argv[15]);
         }
         int id = -1;
-        for (size_t i = 0; i < emberek.get_size(); ++i) {
+        for (int i = 0; i < emberek.get_size(); ++i) {
             if(emberek[i].telefon == argv[9]) {id = i;break;}
         }
         if(id != -1) throw AlreadyExits();
         emberek.push_back(tmp);
     }
 
-
+    //#region File write
     std::ofstream os;
     os.open("../adatok.dat");
     for (size_t i = 0; i < emberek.get_size(); ++i) {
         int meret = 8;
-        if(emberek[i].nev.beceNev != "") meret++;
-        if(emberek[i].privCim.postal != -1) meret+=5;
+        if(emberek[i].nev.get_bece() != "") meret++;
+        if(emberek[i].privCim.get_postal() != -1) meret+=5;
 
         os << meret << " " << emberek[i].file();
         os << "\n";
     }
     os.close();
+    //#endregion
 }
